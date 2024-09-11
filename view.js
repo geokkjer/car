20f8d42ab5b654cb659cc11aa1d0655f178bb0ba
Geir Okkenhaug Jerstad <geir@geokkjer.eu>
2024-09-11 12:49:12 +0200

    added car png

--- view.js
+++ view.js
@@ -1,7 +1,7 @@
 updateView();
 function updateView(){
   app.innerHTML = /*HTML*/ `
-  
+  <img id="car" src="img/car-outlined-vehicle-side-view.png">
   <label for="coolness">Kulhetsmeter: </label>
     <p> <meter min="0" max="100" value="${CoolnessMTR}">${CoolnessMTR}%</meter> </p>
     <button onclick="car()">Kjør</button>
