3446cd0d4534c5769c0b66f7a9d7419e3982197d
Geir Okkenhaug Jerstad <geir@geokkjer.eu>
2024-09-11 12:35:10 +0200

    comments in model

--- model.js
+++ model.js
@@ -1,12 +1,17 @@
+// Appen
 var app = document.getElementById('app');
 
+// Random stuff
 var randomStuffWithCoolScore = {
     spareTire: -10,
     roofRack: 1,
 
 };
+
+// Coolness
 var CoolnessMTR = 50;
 
+// Car
 var car = ['start', 'stop'];
 
 var stuffOnCar = [];
