f3814516cab8f527027ce0597b1eebccbb012213
Geir Okkenhaug Jerstad <geir@geokkjer.eu>
2024-09-11 11:25:56 +0200

    coolnessmeter to view

--- view.js
+++ view.js
@@ -1,6 +1,8 @@
 updateView();
 function updateView(){
-    
+  document.getElementById('app').innerHTML = /*HTML*/ `
+    <p> <meter min="0" max="100" value="50">50%</meter> </p>
+  `;
 }
 function WinAndLoss(){
     
