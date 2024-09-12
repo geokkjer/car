e708a3473d963c5a2d86346bfcb1124dc4605d77
Henrik005 <henrik.mathias.brathen@gmail.com>
2024-09-12 14:04:49 +0200

    Removed Lines

--- view.js
+++ view.js
@@ -1,14 +1,12 @@
 updateView();
 function updateView(){
   app.innerHTML = /*HTML*/ `
-  
   <label for="coolness">Kulhetsmeter: </label>
     <p> <meter min="0" max="100" value="${CoolnessMTR}">${CoolnessMTR}%</meter> </p>
     <button onclick="car()">Kjør</button>
   `;
 }
 function WinAndLoss(){
-
 }
 
 function isDriving(){
